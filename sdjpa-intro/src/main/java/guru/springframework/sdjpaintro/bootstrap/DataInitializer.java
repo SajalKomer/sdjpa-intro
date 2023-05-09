126f8e0fb3582f852dea8c287333c020688f29ef
SajalKomer <sajalkomer@gmail.com>
2023-05-08 22:05:40 -0400

    add datainitialisr and tests

---
+++ sdjpa-intro/src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
@@ -0,0 +1,29 @@
+package guru.springframework.sdjpaintro.bootstrap;
+
+import guru.springframework.sdjpaintro.domain.Book;
+import guru.springframework.sdjpaintro.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Profile({"local", "default"})
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private final BookRepository bookRepository;
+
+    public DataInitializer(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        bookRepository.deleteAll();
+        Book bookDDD = new Book("Domain Driven Design", "7646", "RandomHouse");
+        Book savedDDD = bookRepository.save(bookDDD);
+        Book bookSIA = new Book("Spring In Action", "7333", "Oriely");
+        Book savedSIA = bookRepository.save(bookSIA);
+
+        bookRepository.findAll().forEach(book -> System.out.println(book.getTitle()));
+    }
+}
